54becb2c70cee31a427cf35c89e0dfb8323cc285
Евгений Олянский <evgeniy.olyanskii@gmail.com>
2022-01-13 21:27:04 +0600

    2jsonfile

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
-
-import program from 'commander';
+import { program, Option } from 'commander';
+import genDiff from '../src/genDiff.js';
 
 program
+  .name('gendiff')
+  .usage('[options] <filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format')
-  .argument('<filepath1> <filepath2>');
+  .version('1.0.0')
+  .helpOption('-h, --help', 'output usage information')
+  .addOption(new Option('-f, --format [type]', 'output format').choices(['stylish', 'plain', 'json']).default('stylish'))
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
 
 program.parse();
