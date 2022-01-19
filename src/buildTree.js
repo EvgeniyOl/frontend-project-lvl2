b44fb8c487d8e5bfd76fc5e82df8abe89953126f
Евгений Олянский <evgeniy.olyanskii@gmail.com>
2022-01-19 11:12:58 +0600

    coverage fix

--- src/buildTree.js
+++ src/buildTree.js
@@ -8,18 +8,18 @@ const compareObjects = (object1, object2) => { // сравнение объек�
     if (!_.has(object2, key)) { return { type: 'deleted', key, val: object1[key] }; } // ключ есть только в первом файле
     if (!_.has(object1, key)) { return { type: 'added', key, val: object2[key] }; } // ключ есть только во втором файле
 
-    const collection1 = object1[key]; // коллекция ключей 1 и 2
-    const collection2 = object2[key];
+    const val1 = object1[key]; // коллекция ключей 1 и 2
+    const val2 = object2[key];
 
-    if (_.isPlainObject(collection1) && _.isPlainObject(collection2)) { return { type: 'nested', key, children: compareObjects(collection1, collection2) }; }
+    if (_.isPlainObject(val1) && _.isPlainObject(val2)) { return { type: 'nested', key, children: compareObjects(val1, val2) }; }
     // являются ли они объектами
 
-    if (!_.isEqual(collection1, collection2)) {
+    if (!_.isEqual(val1, val2)) {
       return {
-        type: 'different', key, collection1, collection2, // если одинаковые значения
+        type: 'different', key, val1, val2, // если одинаковые значения
       };
     }
-    return { type: 'same', key, val: collection1 };
+    return { type: 'same', key, val: val1 };
   });
 };
 
